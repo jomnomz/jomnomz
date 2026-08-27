6bbb042b689e06c29200ac87a027cbb3f8642f79
jomnomz <jomeorenzdelacruz@gmail.com>
2026-08-27 22:21:50 +0800

    created project card reusabe component

---
+++ src/sections/projects/projectCard.jsx
@@ -0,0 +1,49 @@
+import Stack from '../../components/ui/stack.jsx'
+
+export default function ProjectCard({projectName,projectDescription, projectType, projectThumbnail,projectLogo, techStack  }) {
+    return(
+        <div className="group">
+            <div className="bg-gray-800 flex justify-center items-center p-2 rounded-tl-lg rounded-tr-lg md:py-9">
+                <div className="w-xs md:w-sm">
+                    <img
+                        className="rounded-sm transition-transform duration-300 group-hover:scale-110"
+                        src={projectThumbnail}
+                        alt="projectThumnail"
+                    />
+                </div>
+            </div>
+            <div className="flex flex-col border rounded-bl-lg rounded-br-lg p-2 md:p-5">
+                <div className="flex flex-col gap-3">
+                    <div className="flex flex-row gap-3">
+                        <div>
+                            <div className="w-12">
+                                <img src={projectLogo} alt="projectLogo" />
+                            </div>
+                        </div>
+                        <div className="flex flex-col justify-center items-start -mt-1">
+                            <div className="text-lg">{projectName}</div>
+                            <div>{projectType}</div>
+                        </div>
+                    </div>
+                    <div className="md:max-w-lg">
+                        <div>{projectDescription}</div>
+                    </div>
+                    <div class="flex flex-row flex-wrap gap-1">
+                        {techStack?.map((stack, index) => (
+                            <Stack
+                                key={index}
+                                color={stack.color}
+                                backgroundColor={stack.backgroundColor}
+                                size={stack.size}
+                                fontSize={stack.fontSize}
+                                img={stack.icon}
+                            >
+                                {stack.name}
+                            </Stack>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/sections/projects/projects.jsx
+++ src/sections/projects/projects.jsx
@@ -2,6 +2,7 @@ import scanpassLogin from '../../assets/scanpassLogin.png'
 import scanpassLogo from '../../assets/scanpassLogo.png'
 import Button from '../../components/ui/button.jsx'
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ProjectCard from './projectCard.jsx'
 export default function Projects() {
     return (
         <div className="md:mx-45">
@@ -14,66 +15,39 @@ export default function Projects() {
             <div className="flex flex-col gap-5 justify-center items-center m-7">
                 <div className="flex flex-col gap-5 md:flex-row">
 
-                    {/* Project Card 1 */}
-                    <div className="group">
-                        <div className="bg-gray-800 flex justify-center items-center p-2 rounded-tl-lg rounded-tr-lg md:py-9">
-                            <div className="w-xs md:w-sm">
-                                <img
-                                    className="rounded-sm transition-transform duration-300 group-hover:scale-110"
-                                    src={scanpassLogin}
-                                    alt="ScanPass login screenshot"
-                                />
-                            </div>
-                        </div>
-                        <div className="flex flex-col border rounded-bl-lg rounded-br-lg p-2 md:p-5">
-                            <div className="flex flex-col gap-3">
-                                <div className="flex flex-row gap-3">
-                                    <div>
-                                        <div className="w-12">
-                                            <img src={scanpassLogo} alt="ScanPass logo" />
-                                        </div>
-                                    </div>
-                                    <div className="flex flex-col justify-center items-start -mt-1">
-                                        <div className="text-lg">ScanPass</div>
-                                        <div>QR Code-Based Attendance Tracking System</div>
-                                    </div>
-                                </div>
-                                <div className="md:max-w-lg">
-                                    <div>ScanPass is a QR code-based attendance system that replaces manual tracking in schools, letting teachers see student time-in/time-out...</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <ProjectCard 
+                        projectThumbnail={scanpassLogin}
+                        projectLogo={scanpassLogo}
+                        projectName="ScanPass" 
+                        projectType="QR Code-Based Attendance Tracking System" 
+                        projectDescription="ScanPass is a QR code-based attendance system that replaces manual tracking in schools, letting teachers see student time-in/time-out..."
+                        techStack={[
+                            {name: "React", color: "white", backgroundColor: "#1A2236", size: "wOIcon", fontSize: "0.8rem"},
+                            {name: "React", color: "white", backgroundColor: "#1A2236", size: "wOIcon", fontSize: "0.8rem"},
+                            {name: "Nex.js", color: "white", backgroundColor: "#1A2236", size: "wOIcon", fontSize: "0.8rem"},
+                            {name: "Express.js", color: "white", backgroundColor: "#1A2236", size: "wOIcon", fontSize: "0.8rem"},
+                            {name: "Supabase", color: "white", backgroundColor: "#1A2236", size: "wOIcon", fontSize: "0.8rem"},
+                            {name: "Vercel", color: "white", backgroundColor: "#1A2236", size: "wOIcon", fontSize: "0.8rem"},
+                            {name: "Github", color: "white", backgroundColor: "#1A2236", size: "wOIcon", fontSize: "0.8rem"},
+                        ]}
+                    />
 
-                    {/* Project Card 2 */}
-                    <div className="group">
-                        <div className="bg-gray-800 flex justify-center items-center p-2 rounded-tl-lg rounded-tr-lg md:py-9">
-                            <div className="w-xs md:w-sm">
-                                <img
-                                    className="rounded-sm transition-transform duration-300 group-hover:scale-110"
-                                    src={scanpassLogin}
-                                    alt="ScanPass login screenshot"
-                                />
-                            </div>
-                        </div>
-                        <div className="flex flex-col border rounded-bl-lg rounded-br-lg p-2 md:p-5">
-                            <div className="flex flex-col gap-3">
-                                <div className="flex flex-row gap-3">
-                                    <div>
-                                        <div className="w-12">
-                                            <img src={scanpassLogo} alt="ScanPass logo" />
-                                        </div>
-                                    </div>
-                                    <div className="flex justify-center items-center">
-                                        <div className="text-lg">ScanPass | QR Code-Based Attendance Tracking System</div>
-                                    </div>
-                                </div>
-                                <div className="md:max-w-lg">
-                                    <div>ScanPass is a QR code-based attendance system that replaces manual tracking in schools, letting teachers see student time-in/time-out...</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <ProjectCard 
+                        projectThumbnail={scanpassLogin}
+                        projectLogo={scanpassLogo}
+                        projectName="ScanPass" 
+                        projectType="QR Code-Based Attendance Tracking System" 
+                        projectDescription="ScanPass is a QR code-based attendance system that replaces manual tracking in schools, letting teachers see student time-in/time-out..."
+                        techStack={[
+                            {name: "React", color: "white", backgroundColor: "#1A2236", size: "wOIcon", fontSize: "0.8rem"},
+                            {name: "React", color: "white", backgroundColor: "#1A2236", size: "wOIcon", fontSize: "0.8rem"},
+                            {name: "Nex.js", color: "white", backgroundColor: "#1A2236", size: "wOIcon", fontSize: "0.8rem"},
+                            {name: "Express.js", color: "white", backgroundColor: "#1A2236", size: "wOIcon", fontSize: "0.8rem"},
+                            {name: "Supabase", color: "white", backgroundColor: "#1A2236", size: "wOIcon", fontSize: "0.8rem"},
+                            {name: "Vercel", color: "white", backgroundColor: "#1A2236", size: "wOIcon", fontSize: "0.8rem"},
+                            {name: "Github", color: "white", backgroundColor: "#1A2236", size: "wOIcon", fontSize: "0.8rem"},
+                        ]}
+                    />
 
                 </div>
             </div>
